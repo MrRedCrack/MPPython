1cd91b6a471df0e3be37c852fe7298909be898c6
Crack <happy1890@gmail.com>
2024-11-07 01:27:30 +0800

    aaaa

--- zshconfig.sh
+++ zshconfig.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 apt update
 apt install -y zsh
-sudo -u pi chsh -s $(which zsh)
+# sudo -u pi chsh -s $(which zsh)
 apt install -y zsh-syntax-highlighting
 # echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+sudo -u pi git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 # echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+sudo -u pi git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 # echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 cp CascadiaCode /usr/share/fonts/truetype -r
 cp MesloLG /usr/share/fonts/truetype -r
-cp -rT zsh ~
+sudo -u pi cp -rT zsh/. ~
 fc-cache -vf /usr/share/fonts/

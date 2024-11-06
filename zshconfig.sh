e2cb28df4fef235de22a631975076bab152177c4
Crack <happy1890@gmail.com>
2024-11-06 23:46:53 +0800

    modify script

--- zshconfig.sh
+++ zshconfig.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-apt-get update && sudo apt-get install zsh
+apt update
+apt install zsh -y
 chsh -s $(which zsh)
-
-apt install zsh-syntax-highlighting
+apt install zsh-syntax-highlighting -y
 echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-
 cp CascadiaCode /usr/share/fonts/truetype -r
 cp MesloLG /usr/share/fonts/truetype -r
 fc-cache -vf /usr/share/fonts/

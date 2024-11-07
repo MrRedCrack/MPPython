9e33fdd643b0663b701699319ceb8a35420f6e3a
Crack <happy1890@gmail.com>
2024-11-07 09:00:45 +0800

    reeee

--- zshconfig.sh
+++ zshconfig.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
-usr=pi
+H=~pi
 apt update
 apt install -y zsh
 # sudo -u pi chsh -s $(which zsh)
 apt install -y zsh-syntax-highlighting
 # echo "source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~$pi/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions $H/.zsh/zsh-autosuggestions
 # echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~$pi/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $H/powerlevel10k
 # echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
 cp CascadiaCode /usr/share/fonts/truetype -r
 cp MesloLG /usr/share/fonts/truetype -r
-cp -rT zsh/. ~$pi
+cp -rT zsh/. $H
 fc-cache -vf /usr/share/fonts/
 if [[ $SHELL != $(which zsh) ]]
 then
-    chsh -s $(which zsh)
-    sudo -u pi sudo -u pi chsh -s $(which zsh)
+    chsh -s $(which zsh) pi
 fi
